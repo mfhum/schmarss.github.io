9ef7ea42476c15b98e30a243816943ed36809875
Marius Hummel <49985831+schmarss@users.noreply.github.com>
2019-10-21 14:33:48 +0200

    Adding Dark mode function

--- js/menu.js
+++ js/menu.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function($){
 		$(window).on('scroll',
 		{
 	        previousTop: 0
-	    }, 
+	    },
 	    function () {
 		    var currentTop = $(window).scrollTop();
 		    //check if user is scrolling up
@@ -26,13 +26,46 @@ jQuery(document).ready(function($){
 		    }
 		    this.previousTop = currentTop;
 		});
+
+			}
+
+	if( document.getElementById('myonoffswitch').checked ) {
+		document.getElementById("myonoffswitch").checked = true;
+		checkdarkmode( true );
+	} else if( !document.getElementById('myonoffswitch').checked ) {
+		document.getElementById("myonoffswitch").checked = false;
+		checkdarkmode( false );
+
 	}
+	$('body').delay(550).css({
+			'overflow': 'visible'
+	})
+
+	// change darkmodetof if clicked
+	$('.onoffswitch-checkbox').on('click', function(){
+		if( document.getElementById('myonoffswitch').checked ) {
+		  checkdarkmode( true );
+		} else if( !document.getElementById('myonoffswitch').checked ) {
+			checkdarkmode( false );
+	 }
+	});
+
+	function checkdarkmode( darkmodecheck ) {
+		if( darkmodecheck ) {
+	    getDarkmode();
+		} else if( !darkmodecheck ) {
+			getLightmode();
+		}
+	}
+
+
+
 
 	//open/close primary navigation
 	$('.box-primary-nav-trigger').on('click', function(){
-		$('.box-menu-icon').toggleClass('is-clicked'); 
+		$('.box-menu-icon').toggleClass('is-clicked');
 		$('.box-header').toggleClass('menu-is-open');
-		
+
 		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
 		if( $('.box-primary-nav').hasClass('is-visible') ) {
 			$('.box-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
@@ -41,7 +74,34 @@ jQuery(document).ready(function($){
 		} else {
 			$('.box-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
 				$('body').addClass('overflow-hidden');
-			});	
+			});
 		}
 	});
 });
+
+
+
+
+// changes to darkmode
+function getDarkmode(){
+	console.log( "Darkmode" );
+	$('body').addClass('is-dark');
+	$('.box-words-wrapper').addClass('is-dark');
+	$('.box-logo-light').addClass('is-dark');
+	$('.box-logo-dark').addClass('is-dark');
+	$('h1').addClass('is-dark');
+	$('p').addClass('is-dark');
+	$('a').addClass('is-dark');
+}
+
+// changes to lightmode
+function getLightmode(){
+	console.log( "Lightmode" );
+	$('body').removeClass('is-dark');
+	$('.box-words-wrapper').removeClass('is-dark');
+	$('.box-logo-light').removeClass('is-dark');
+	$('.box-logo-dark').removeClass('is-dark');
+	$('h1').removeClass('is-dark');
+	$('p').removeClass('is-dark');
+	$('a').removeClass('is-dark');
+}
